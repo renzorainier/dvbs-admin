313f014013b8cd50487cd10c2d85843fa87f580e
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-04-05 17:40:35 +0000

    Fri, Apr 5, 2024, 5:40 PM +00:00

--- src/app/Visitors.jsx
+++ src/app/Visitors.jsx
@@ -52,11 +52,10 @@ function Visitors() {
 const addVisitor = async () => {
   if (newVisitorName.trim() !== "") {
     try {
+      // Add a new document with a randomly generated ID
       const docRef = await addDoc(collection(db, "visitors"), {
-        [newVisitorName]: {
-          name: newVisitorName,
-          [currentWeekNumber]: true,
-        }
+        visitorName: newVisitorName, // Store the name of the visitor in a separate field
+        [currentWeekNumber]: true,
       });
 
       const newVisitor = { id: docRef.id, name: newVisitorName };
