e2045bbe8cf1fd71990616367f39d21ed63642de
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-04-05 18:12:59 +0000

    Fri, Apr 5, 2024, 6:12 PM +00:00

--- src/app/Visitors.jsx
+++ src/app/Visitors.jsx
@@ -6,81 +6,63 @@ function Visitors() {
   // State Variables
   const [visitors, setVisitors] = useState([]);
   const [recentVisitors, setRecentVisitors] = useState([]);
-  const [olderVisitors, setOlderVisitors] = useState([]);
   const [newVisitorName, setNewVisitorName] = useState("");
   const [currentWeekNumber, setCurrentWeekNumber] = useState(getWeekNumber());
 
-  // Fetch Visitors on Load
   useEffect(() => {
-    const fetchVisitors = async () => {
+    fetchVisitors();
+  }, [currentWeekNumber]);
+
+  const fetchVisitors = async () => {
+    try {
       const visitorsSnapshot = await getDocs(collection(db, "visitors"));
       const visitorList = visitorsSnapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
-
-      // Filter recent and older visitors
-      const recentWeeksThreshold = currentWeekNumber - 4;
-      const recentVisitors = visitorList.filter((visitor) => {
-        const highestWeek = Math.max(
-          ...Object.keys(visitor).filter((key) => !isNaN(key))
-        );
-        return highestWeek >= recentWeeksThreshold;
-      });
-      const olderVisitors = visitorList.filter((visitor) => {
-        const highestWeek = Math.max(
-          ...Object.keys(visitor).filter((key) => !isNaN(key))
-        );
-        return highestWeek < recentWeeksThreshold;
-      });
-
       setVisitors(visitorList);
-      setRecentVisitors(recentVisitors);
-      setOlderVisitors(olderVisitors);
-    };
+      filterVisitors(visitorList);
+    } catch (error) {
+      console.error("Error fetching visitors: ", error);
+    }
+  };
 
-    fetchVisitors();
-  }, [currentWeekNumber]);
+  const filterVisitors = (visitorList) => {
+    const recentWeeksThreshold = currentWeekNumber - 4;
+    const recentVisitors = visitorList.filter((visitor) => {
+      const highestWeek = Math.max(
+        ...Object.keys(visitor).filter((key) => !isNaN(key))
+      );
+      return highestWeek >= recentWeeksThreshold;
+    });
+    setRecentVisitors(recentVisitors);
+  };
 
-  // Handle Input Change
   const handleInputChange = (event) => {
     setNewVisitorName(event.target.value);
   };
 
-  // Add a New Visitor
   const addVisitor = async () => {
     if (newVisitorName.trim() !== "") {
       try {
-        // Set the document name to match the visitor's name
         const docRef = doc(collection(db, "visitors"), newVisitorName.trim());
-        // Check if the document already exists
         const docSnapshot = await getDoc(docRef);
         if (docSnapshot.exists()) {
           console.error("Visitor already exists!");
           return;
         }
-        // If the document doesn't exist, add it
         await setDoc(docRef, {
-        name: newVisitorName, // Store the name of the visitor in a separate field
+          name: newVisitorName,
           [currentWeekNumber]: true,
         });
-
-        const newVisitor = { id: docRef.id, name: newVisitorName };
-        setVisitors([...visitors, newVisitor]);
         setNewVisitorName("");
-
-        // Update recent visitors by prepending the new visitor
-        setRecentVisitors((prevRecentVisitors) => [newVisitor, ...prevRecentVisitors]);
-
-        // Add console log to check if recent visitor is updated
-        console.log("Recent Visitors After Adding:", recentVisitors);
+        fetchVisitors();
       } catch (error) {
         console.error("Error adding visitor: ", error);
       }
     }
   };
 
-  // Handle click on a visitor name
   const handleVisitorClick = async (visitorId) => {
     try {
       const docRef = doc(db, "visitors", visitorId);
@@ -95,29 +77,7 @@ function Visitors() {
         [timeField]: !isVisitorPresent ? uploadTime : "",
       });
 
-      // Update local state for immediate feedback
-      setVisitors((prevVisitors) =>
-        prevVisitors.map((visitor) =>
-          visitor.id === visitorId
-            ? {
-                ...visitor,
-                [currentWeekNumber]: !isVisitorPresent,
-              }
-            : visitor
-        )
-      );
-
-      // Update recent visitors if necessary
-      setRecentVisitors((prevRecentVisitors) =>
-        prevRecentVisitors.map((visitor) =>
-          visitor.id === visitorId
-            ? {
-                ...visitor,
-                [currentWeekNumber]: !isVisitorPresent,
-              }
-            : visitor
-        )
-      );
+      fetchVisitors();
     } catch (error) {
       console.error("Error updating Firebase: ", error);
     }
@@ -139,59 +99,55 @@ function Visitors() {
     );
   }
 
-  // Rendering Section
   return (
     <div className="flex flex-col items-center">
       <div className="flex flex-col gap-2 w-full">
         <h3>Recent Visitors:</h3>
         <div className="flex flex-col gap-2">
           {recentVisitors.map((visitor) => (
-            <button
-              key={visitor.id}
-              className={`font-bold py-3 px-4 rounded-xl text-lg sm:text-xl md:text-2xl ${
-                visitor[currentWeekNumber] ? "bg-green-500" : "bg-gray-500 hover:bg-blue-700"
-              } text-white`}
-              onClick={() => handleVisitorClick(visitor.id)}
-            >
-              {visitor.name}
-            </button>
-          ))}
-        </div>
-      </div>
-      <div className="flex flex-col gap-2 w-full">
-        <h3>Older Visitors:</h3>
-        <div className="flex flex-col gap-2">
-          {olderVisitors.map((visitor) => (
-            <button
-              key={visitor.id}
-              className={`font-bold py-3 px-4 rounded-xl text-lg sm:text-xl md:text-2xl ${
-                visitor[currentWeekNumber] ? "bg-green-500" : "bg-gray-500 hover:bg-blue-700"
-              } text-white`}
-              onClick={() => handleVisitorClick(visitor.id)}
-            >
-              {visitor.name}
-            </button>
+            <VisitorButton key={visitor.id} visitor={visitor} onClick={handleVisitorClick} />
           ))}
         </div>
       </div>
 
-      <div className="flex flex-col items-center gap-2 justify-center mt-6 bg-gray-100 border border-gray-300 rounded-md p-4">
-        <input
-          type="text"
-          value={newVisitorName}
-          onChange={handleInputChange}
-          placeholder="Enter visitor name"
-          className="border border-gray-400 rounded p-3 w-64"
-        />
-        <button
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-4 rounded mx-2"
-          onClick={addVisitor}
-        >
-          Add Visitor
-        </button>
-      </div>
+      <VisitorInput newVisitorName={newVisitorName} onChange={handleInputChange} onClick={addVisitor} />
+
     </div>
   );
 }
+
+const VisitorButton = ({ visitor, onClick }) => {
+  const currentWeekNumber = getWeekNumber();
+  return (
+    <button
+      className={`font-bold py-3 px-4 rounded-xl text-lg sm:text-xl md:text-2xl ${
+        visitor[currentWeekNumber] ? "bg-green-500" : "bg-gray-500 hover:bg-blue-700"
+      } text-white`}
+      onClick={() => onClick(visitor.id)}
+    >
+      {visitor.name}
+    </button>
+  );
+};
+
+const VisitorInput = ({ newVisitorName, onChange, onClick }) => {
+  return (
+    <div className="flex flex-col items-center gap-2 justify-center mt-6 bg-gray-100 border border-gray-300 rounded-md p-4">
+      <input
+        type="text"
+        value={newVisitorName}
+        onChange={onChange}
+        placeholder="Enter visitor name"
+        className="border border-gray-400 rounded p-3 w-64"
+      />
+      <button
+        className="bg-green-500 hover:bg-green-700 text-white font-bold py-3 px-4 rounded mx-2"
+        onClick={onClick}
+      >
+        Add Visitor
+      </button>
+    </div>
+  );
+};
 
 export default Visitors;
