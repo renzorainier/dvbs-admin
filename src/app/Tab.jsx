728d0e073cc422a57b771c528e8e1f73ba0fac90
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-04-06 15:14:55 +0000

    Sat, Apr 6, 2024, 3:14 PM +00:00

--- src/app/Tab.jsx
+++ src/app/Tab.jsx
@@ -8,6 +8,7 @@ import Members from "./Members";
 
 function Tab() {
   const [state, setState] = useState(false);
+
   return (
     <div>
       <div className="flex justify-center py-5 items-center">
@@ -17,15 +18,15 @@ function Tab() {
             onChange={setState}
             className={`${
               state ? "bg-violet-400" : "bg-blue-400"
-            } relative inline-flex h-[50px] w-full shrink-0 cursor-pointer rounded-lg border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
-          >
+            } relative inline-flex h-[50px] w-full shrink-0 cursor-pointer rounded-lg border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}>
             <span
               aria-hidden="true"
               className={`${
                 state ? "translate-x-full" : "translate-x-0"
-              } pointer-events-none inline-block h-[47px] w-[50%] transform rounded-lg bg-gray-100 shadow-lg ring-0 transition duration-200 ease-in-out`}
+              } pointer-events-none inline-block h-[47px] w-[50%] transform rounded-lg bg-gray-100 shadow-lg ring-0 transition duration-200 ease-in-out `}
             />
-            <div className="absolute top-1/2 left-[12%] transform -translate-y-1/2 font-bold text-lg">
+
+            <div className="absolute  top-1/2 left-[12%] transform -translate-y-1/2 font-bold text-lg">
               Members
             </div>
             <div className="absolute top-1/2 right-[18%] transform -translate-y-1/2 font-bold text-lg">
@@ -34,27 +35,19 @@ function Tab() {
           </Switch>
         </div>
       </div>
-      <div className="flex justify-center py-5 items-center">
-        <div className="absolute mt-2 origin-top divide-y divide-gray-100 rounded-lg bg-gradient-to-b from-gray-100 to-white shadow-xl ring-1 ring-black/5 focus:outline-none flex flex-col items-center">
-          <div className="w-full divide-y divide-gray-100">
-            <div className="w-full">
-              {state ? (
-                <Members members={firstColumn} />
-              ) : (
-                <Visitors />
-              )}
-            </div>
-            <div className="w-full">
-              {state ? (
-                <Members members={secondColumn} />
-              ) : null}
-            </div>
+      <div>
+        {state ? (
+          <div>
+            <Visitors />
           </div>
-        </div>
+        ) : (
+          <div>
+              <Members/>
+          </div>
+        )}
       </div>
     </div>
   );
-
 }
 
 export default Tab;
