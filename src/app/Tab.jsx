5d885b5d8a2c4b610ea9030475c2e1728be7a046
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-04-06 16:16:30 +0000

    Sat, Apr 6, 2024, 4:16 PM +00:00

--- src/app/Tab.jsx
+++ src/app/Tab.jsx
@@ -12,11 +12,12 @@ function Tab() {
   return (
     <div style={{ height: '100vh' }}>
       <div className="flex justify-center pt-7 pb-4 items-center">
-        <div className="w-full rounded-lg mx-auto ">
+        <div className="w-full rounded-lg mx-auto " >
           <Switch
             checked={state}
             onChange={setState}
-            className="relative inline-flex h-[50px] w-full shrink-0 cursor-pointer rounded-lg border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75}"
+            className="relative inline-flex h-[50px] w-full shrink-0 cursor-pointer rounded-lg border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75}
+            "
             style={{
               // Translate Tailwind classes to inline styles
               marginTop: "-0.5rem", // equivalent to mt-2
@@ -24,10 +25,11 @@ function Tab() {
               borderBottomWidth: "1px", // equivalent to divide-y
               borderColor: "#E5E7EB", // equivalent to divide-gray-100
               borderRadius: "0.5rem", // equivalent to rounded-lg
+
               boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.1), 0px 4px 6px -2px rgba(0,0,0,0.05)", // equivalent to shadow-xl
               ringWidth: "1px", // equivalent to ring-1
               ringColor: "rgba(0,0,0,0.2)", // equivalent to ring-black/5
-              outline: "none", // equivalent to focus:outline-none
+              outline: "none" // equivalent to focus:outline-none
             }}
           >
             <span
@@ -37,10 +39,10 @@ function Tab() {
               } pointer-events-none inline-block h-[47px] w-[50%] transform rounded-lg bg-gray-100 shadow-lg ring-0 transition duration-200 ease-in-out `}
             />
 
-            <div className="absolute top-1/2 left-[50%] transform -translate-x-1/2 -translate-y-1/2 font-bold text-4xl">
+            <div  className="absolute  top-1/2 left-[4%] transform -translate-y-1/2 font-bold text-4xl">
               Members
             </div>
-            <div className="absolute top-1/2 right-[50%] transform translate-x-1/2 -translate-y-1/2 font-bold text-4xl">
+            <div className="absolute top-1/2 right-[9%] transform -translate-y-1/2 font-bold text-4xl">
               Visitors
             </div>
           </Switch>
@@ -59,7 +61,6 @@ function Tab() {
       </div>
     </div>
   );
-
 
 
 }
