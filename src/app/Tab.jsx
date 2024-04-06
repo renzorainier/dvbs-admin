842145348dac761da3acc3c7fa79f46eeed5e149
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-04-06 15:29:22 +0000

    Sat, Apr 6, 2024, 3:29 PM +00:00

--- src/app/Tab.jsx
+++ src/app/Tab.jsx
@@ -11,14 +11,13 @@ function Tab() {
 
   return (
     <div>
-      <div className="flex justify-center py-5 items-center">
+      <div className="flex justify-center pt-7 pb-4 items-center">
         <div className="w-full rounded-lg mx-auto">
           <Switch
             checked={state}
             onChange={setState}
-            className={`${
-              state ? "bg-[A2C579]" : "bg-blue-400"
-            } relative inline-flex h-[50px] w-full shrink-0 cursor-pointer rounded-lg border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
+            className="relative inline-flex h-[50px] w-full shrink-0 cursor-pointer rounded-lg border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75}
+            "
             style={{
               // Translate Tailwind classes to inline styles
               marginTop: "-0.5rem", // equivalent to mt-2
@@ -26,7 +25,7 @@ function Tab() {
               borderBottomWidth: "1px", // equivalent to divide-y
               borderColor: "#E5E7EB", // equivalent to divide-gray-100
               borderRadius: "0.5rem", // equivalent to rounded-lg
-              background: "rgba(66, 74, 87, 0.2)", // semi-transparent with a dark shade
+            
               boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.1), 0px 4px 6px -2px rgba(0,0,0,0.05)", // equivalent to shadow-xl
               ringWidth: "1px", // equivalent to ring-1
               ringColor: "rgba(0,0,0,0.2)", // equivalent to ring-black/5
