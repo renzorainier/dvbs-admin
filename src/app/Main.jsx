56763ad4c72d00aaaa5a6ff4b274a81f766f4679
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-15 05:14:34 +0000

    Wed, May 15, 2024, 5:14 AM +00:00

--- src/app/Main.jsx
+++ src/app/Main.jsx
@@ -4,13 +4,14 @@ import React from "react";
 import Visitors from "./Visitors";
 import Tab from "./Tab";
 import Primary from "./Primary"
-import IniitializeData from "./InitializeData"
+import InitializeData from "./InitializeData"
 
 
 function Main() {
   return (
     <div>
       <Tab/>
+      <InitializeData/>
     </div>
   );
 }
