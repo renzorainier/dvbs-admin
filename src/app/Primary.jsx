6d26c4fd136fe592f9c3605a741650b1f8fddfdc
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-15 07:38:40 +0000

    Wed, May 15, 2024, 7:38 AM +00:00

--- src/app/Primary.jsx
+++ src/app/Primary.jsx
@@ -87,46 +87,7 @@ function Primary() {
 
   return (
     <div>
-      <div className="flex justify-center mb-5">
-        <div className="flex items-center bg-white border rounded-lg shadow-md p-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 text-[#FFC100]">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632"
-            />
-          </svg>
 
-          <p className="text-gray-800 font-bold  ml-2 text-sm sm:text-base md:text-lg lg:text-xl">
-            {countPresentForToday()}
-          </p>
-        </div>
-        <div className="flex items-center bg-white border rounded-lg shadow-md p-4 ml-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 text-[#FFC100]">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
-          </svg>
-
-          <p className="text-gray-800 font-bold  ml-2 text-lg sm:text-base md:text-lg lg:text-xl">
-            {countAbsentForToday()}
-          </p>
-        </div>
-      </div>
 
       <div className="flex flex-col items-center">
         <div className="w-full text-gray-700 bg-white p-5 border rounded-lg shadow-lg mx-auto">
@@ -163,3 +124,51 @@ function Primary() {
 }
 
 export default Primary;
+
+
+
+
+
+
+
+
+// <div className="flex justify-center mb-5">
+// <div className="flex items-center bg-white border rounded-lg shadow-md p-4">
+//   <svg
+//     xmlns="http://www.w3.org/2000/svg"
+//     fill="none"
+//     viewBox="0 0 24 24"
+//     strokeWidth={1.5}
+//     stroke="currentColor"
+//     className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 text-[#FFC100]">
+//     <path
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632"
+//     />
+//   </svg>
+
+//   <p className="text-gray-800 font-bold  ml-2 text-sm sm:text-base md:text-lg lg:text-xl">
+//     {countPresentForToday()}
+//   </p>
+// </div>
+// <div className="flex items-center bg-white border rounded-lg shadow-md p-4 ml-4">
+//   <svg
+//     xmlns="http://www.w3.org/2000/svg"
+//     fill="none"
+//     viewBox="0 0 24 24"
+//     strokeWidth={1.5}
+//     stroke="currentColor"
+//     className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 text-[#FFC100]">
+//     <path
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       d="M6 18L18 6M6 6l12 12"
+//     />
+//   </svg>
+
+//   <p className="text-gray-800 font-bold  ml-2 text-lg sm:text-base md:text-lg lg:text-xl">
+//     {countAbsentForToday()}
+//   </p>
+// </div>
+// </div>
