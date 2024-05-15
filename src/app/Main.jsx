f07aef290ded7013bb58f6cbd87cdbe98417bda4
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-15 03:50:01 +0000

    Wed, May 15, 2024, 3:50 AM +00:00

--- src/app/Main.jsx
+++ src/app/Main.jsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import Fetch from "./Fetch";
 import Upload from "./Upload";
 import Visitors from "./Visitors";
 import Tab from "./Tab";
@@ -12,9 +11,7 @@ import IniitializeData from "./InitializeData"
 function Main() {
   return (
     <div>
-      {/* <Fetch />
-      <Visitors/> */}
-      {/* <Upload /> */}
+
       <Tab/>
       <IniitializeData/>
 
