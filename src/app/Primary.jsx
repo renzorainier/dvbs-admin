9af093a839d3e72f1a7049676e60f3b54b21c489
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-15 07:09:44 +0000

    Wed, May 15, 2024, 7:09 AM +00:00

--- src/app/Primary.jsx
+++ src/app/Primary.jsx
@@ -22,7 +22,7 @@ function Primary() {
   }, []);
 
   const getCurrentDayLetter = () => {
-    const days = ['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+    const days = ["A", "B", "C", "D", "E", "F", "G"];
     const dayIndex = new Date().getDay(); // 0 is Sunday, 1 is Monday, ..., 6 is Saturday
     return days[dayIndex === 0 ? 6 : dayIndex - 1]; // Adjust to make A = Monday
   };
@@ -59,51 +59,72 @@ function Primary() {
   // Function to count the number of present students for the current day
   const countPresentForToday = () => {
     const dayLetter = getCurrentDayLetter();
-    return Object.keys(primaryData).filter(key => key.endsWith(dayLetter) && primaryData[key]).length;
+    return Object.keys(primaryData).filter(
+      (key) => key.endsWith(dayLetter) && primaryData[key]
+    ).length;
   };
 
   // Function to count the number of absent students for the current day
   const countAbsentForToday = () => {
     const dayLetter = getCurrentDayLetter();
-    const totalStudents = Object.keys(primaryData).filter(key => key.endsWith(dayLetter)).length;
+    const totalStudents = Object.keys(primaryData).filter((key) =>
+      key.endsWith(dayLetter)
+    ).length;
     const presentCount = countPresentForToday();
     return totalStudents - presentCount;
   };
 
   // Extract and sort the "name" fields alphabetically
   const sortedNames = Object.keys(primaryData)
-    .filter(fieldName => fieldName.endsWith("name"))
-    .map(fieldName => primaryData[fieldName])
+    .filter((fieldName) => fieldName.endsWith("name"))
+    .map((fieldName) => primaryData[fieldName])
     .sort();
 
   // Filter the sortedNames based on the search query
-  const filteredNames = sortedNames.filter(name =>
+  const filteredNames = sortedNames.filter((name) =>
     name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
   return (
     <div className="flex flex-col items-center">
-          <div>
-          <div className="flex items-center bg-white border rounded-lg shadow-md p-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 text-[#A2C579]"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632"
-                />
-              </svg>
-              <p className="text-gray-800 font-bold  ml-2 text-lg sm:text-base md:text-lg lg:text-xl">
-              {countPresentForToday()}
-              </p>
-            </div>
-          </div>
+      <div>
+        <div className="flex items-center bg-white border rounded-lg shadow-md p-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 text-[#D2DE32]">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"
+            />
+          </svg>
+          <p className="text-gray-800 font-bold  ml-2 text-sm sm:text-base md:text-lg lg:text-xl">
+            {countPresentForToday()}
+          </p>
+        </div>
+        <div className="flex items-center bg-white border rounded-lg shadow-md p-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 text-[#A2C579]">
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632"
+            />
+          </svg>
+          <p className="text-gray-800 font-bold  ml-2 text-lg sm:text-base md:text-lg lg:text-xl">
+            {countAbsentForToday()}
+          </p>
+        </div>
+      </div>
       <div className="w-full text-gray-700 bg-white p-5 border rounded-lg shadow-lg mx-auto">
         <input
           type="text"
@@ -112,20 +133,20 @@ function Primary() {
           onChange={(e) => setSearchQuery(e.target.value)}
           className="w-full p-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500"
         />
-        <div className="flex justify-between items-center mb-4">
-         
-          <h2 className="text-lg font-bold">Total Absent Today: {countAbsentForToday()}</h2>
-        </div>
+        <div className="flex justify-between items-center mb-4"></div>
 
         <div className="flex flex-col gap-2 w-full">
           {filteredNames.map((name, index) => {
-            const fieldName = Object.keys(primaryData).find(key => primaryData[key] === name);
+            const fieldName = Object.keys(primaryData).find(
+              (key) => primaryData[key] === name
+            );
             return (
               <button
                 key={index}
-                className={`hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-xl ${getButtonColor(fieldName)}`}
-                onClick={() => handleClick(fieldName)}
-              >
+                className={`hover:bg-blue-700 text-white font-bold py-3 px-4 rounded-xl ${getButtonColor(
+                  fieldName
+                )}`}
+                onClick={() => handleClick(fieldName)}>
                 {name}
               </button>
             );
