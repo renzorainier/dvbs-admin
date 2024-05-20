c862d2c5c911b49254e87bcc7206ca13e0805524
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-20 04:00:43 +0000

    Mon, May 20, 2024, 4:00 AM +00:00

--- src/app/StudentOutTime.jsx
+++ src/app/StudentOutTime.jsx
@@ -112,54 +112,57 @@ function StudentOutTime() {
   return (
     <div className="flex flex-col items-center">
       <h1 className="text-xl font-bold mb-4">Present Students</h1>
-      <Menu as="div" className="relative inline-block text-left w-full mb-4">
-          <div>
-            <Menu.Button className="inline-flex w-full justify-between rounded-md bg-black/20 px-4 py-2 text-sm font-bold text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
-              <span>{selectedLocation || "All Locations"}</span>
-              <ChevronDownIcon className="ml-2 -mr-1 h-5 w-5" aria-hidden="true" />
-            </Menu.Button>
-          </div>
-          <Transition
-            as={Fragment}
-            enter="transition ease-out duration-100"
-            enterFrom="transform opacity-0 scale-95"
-            enterTo="transform opacity-100 scale-100"
-            leave="transition ease-in duration-75"
-            leaveFrom="transform opacity-100 scale-100"
-            leaveTo="transform opacity-0 scale-95"
-          >
-            <Menu.Items className="absolute z-10 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-              <div className="py-1">
-                <Menu.Item>
-                  {({ active }) => (
-                    <button
-                      className={`${
-                        active ? "bg-gray-100 text-gray-900" : "text-gray-700"
-                      } block px-4 py-2 text-sm w-full text-left`}
-                      onClick={() => handleLocationChange("")}
-                    >
-                      All Locations
-                    </button>
-                  )}
-                </Menu.Item>
-                {locations.map((location) => (
-                  <Menu.Item key={location}>
-                    {({ active }) => (
-                      <button
-                        className={`${
-                          active ? "bg-gray-100 text-gray-900" : "text-gray-700"
-                        } block px-4 py-2 text-sm w-full text-left`}
-                        onClick={() => handleLocationChange(location)}
-                      >
-                        {location}
-                      </button>
-                    )}
-                  </Menu.Item>
-                ))}
-              </div>
-            </Menu.Items>
-          </Transition>
-        </Menu>
+      <Menu as="div" className="relative inline-block justify-center text-center mt-4">
+  <div>
+    <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-bold text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
+      <h2 className="text-4xl font-bold">
+        {selectedLocation || "All Locations"}
+      </h2>
+      <ChevronDownIcon className="ml-2 -mr-1 h-10 w-10" aria-hidden="true" />
+    </Menu.Button>
+  </div>
+  <Transition
+    as={Fragment}
+    enter="transition ease-out duration-100"
+    enterFrom="transform opacity-0 scale-95"
+    enterTo="transform opacity-100 scale-100"
+    leave="transition ease-in duration-75"
+    leaveFrom="transform opacity-100 scale-100"
+    leaveTo="transform opacity-0 scale-95"
+  >
+    <Menu.Items className="absolute z-10 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+      <div className="py-1">
+        <Menu.Item>
+          {({ active }) => (
+            <button
+              className={`${
+                active ? "bg-gray-100 text-gray-900" : "text-gray-700"
+              } block px-4 py-2 text-2xl font-semibold w-full text-left`}
+              onClick={() => handleLocationChange("")}
+            >
+              All Locations
+            </button>
+          )}
+        </Menu.Item>
+        {locations.map((location) => (
+          <Menu.Item key={location}>
+            {({ active }) => (
+              <button
+                className={`${
+                  active ? "bg-gray-100 text-gray-900" : "text-gray-700"
+                } block px-4 py-2 text-2xl font-semibold w-full text-left`}
+                onClick={() => handleLocationChange(location)}
+              >
+                {location}
+              </button>
+            )}
+          </Menu.Item>
+        ))}
+      </div>
+    </Menu.Items>
+  </Transition>
+</Menu>
+
       <div className="w-full max-w-md text-gray-700 bg-white p-5 border rounded-lg shadow-lg mx-auto">
 
         {filteredStudents.map((student) => (
