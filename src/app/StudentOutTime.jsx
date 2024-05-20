59118d38df403ed22f689601948750e44bdceeaa
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-20 05:51:30 +0000

    Mon, May 20, 2024, 5:51 AM +00:00

--- src/app/StudentOutTime.jsx
+++ src/app/StudentOutTime.jsx
@@ -209,14 +209,51 @@ function StudentOutTime() {
         </Transition>
       </Menu>
 
-      <div className="flex items-center justify-between mb-4">
-          <div>
-            <span className="font-bold">Marked:</span> {markedCount}
-          </div>
-          <div>
-            <span className="font-bold">Not Marked:</span> {notMarkedCount}
-          </div>
+      <div className="flex justify-center mb-5 font-bold">
+        <div className="flex items-center bg-white border rounded-lg shadow-md p-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632"
+            />
+          </svg>
+
+          <p className="text-gray-800 font-bold  ml-2 text-lg sm:text-base md:text-lg lg:text-xl">
+          {markedCount}
+          </p>
         </div>
+        <div className="flex items-center bg-white border rounded-lg shadow-md p-4 ml-4">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+
+          <p className="text-gray-800 font-bold  ml-2 text-lg sm:text-base md:text-lg lg:text-xl">
+          {notMarkedCount}
+          </p>
+        </div>
+
+        </div>
+
+
       <div className="w-full max-w-md text-gray-700 bg-white mt-5 p-5 border rounded-lg shadow-lg mx-auto">
         <input
           type="text"
