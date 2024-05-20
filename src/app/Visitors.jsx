9b0d03ce7ccb4a9df805720b7964456261cfa706
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-20 04:24:37 +0000

    Mon, May 20, 2024, 4:24 AM +00:00

--- src/app/Visitors.jsx
+++ src/app/Visitors.jsx
@@ -264,7 +264,7 @@ function Visitors({ config, currentConfigIndex, setCurrentConfigIndex }) {
               type="text"
               value={contactNumber}
               onChange={(e) => handleInputChange(e, "contactNumber")}
-              placeholder="Contact Number"
+              placeholder="Contact Number (optional)"
               className="border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:border-[${config.color}]"
             />
             <button
