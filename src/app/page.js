c3b0aebb064db8a1576c9f74402aadd400f645a5
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-21 13:34:39 +0000

    Tue, May 21, 2024, 1:34 PM +00:00

--- src/app/page.js
+++ src/app/page.js
@@ -5,38 +5,38 @@ import Head from "next/head.js";
 import React, { useState, Fragment } from "react";
 
 export default function Home() {
-  const [currentConfigIndex, setCurrentConfigIndex] = useState(0);
+  // const [currentConfigIndex, setCurrentConfigIndex] = useState(0);
 
-  const configurations = [
-    {
-      name: "Primary",
-      colors: { present: "bg-[#FFC100]", absent: "bg-gray-400" },
-      dbPath: "dvbs/primary",
-      color: "#FFC100",
-      ageRange: [4, 5, 6],
-    },
-    {
-      name: "Middlers",
-      colors: { present: "bg-[#04d924]", absent: "bg-gray-500" },
-      dbPath: "dvbs/middlers",
-      color: "#04d924",
-      ageRange: [7, 8, 9],
-    },
-    {
-      name: "Juniors",
-      colors: { present: "bg-[#027df7]", absent: "bg-gray-500" },
-      dbPath: "dvbs/juniors",
-      color: "#027df7",
-      ageRange: [10, 11, 12],
-    },
-    {
-      name: "Youth",
-      colors: { present: "bg-[#f70233]", absent: "bg-gray-500" },
-      dbPath: "dvbs/youth",
-      color: "#f70233",
-      ageRange: [13, 14, 15],
-    },
-  ];
+  // const configurations = [
+  //   {
+  //     name: "Primary",
+  //     colors: { present: "bg-[#FFC100]", absent: "bg-gray-400" },
+  //     dbPath: "dvbs/primary",
+  //     color: "#FFC100",
+  //     ageRange: [4, 5, 6],
+  //   },
+  //   {
+  //     name: "Middlers",
+  //     colors: { present: "bg-[#04d924]", absent: "bg-gray-500" },
+  //     dbPath: "dvbs/middlers",
+  //     color: "#04d924",
+  //     ageRange: [7, 8, 9],
+  //   },
+  //   {
+  //     name: "Juniors",
+  //     colors: { present: "bg-[#027df7]", absent: "bg-gray-500" },
+  //     dbPath: "dvbs/juniors",
+  //     color: "#027df7",
+  //     ageRange: [10, 11, 12],
+  //   },
+  //   {
+  //     name: "Youth",
+  //     colors: { present: "bg-[#f70233]", absent: "bg-gray-500" },
+  //     dbPath: "dvbs/youth",
+  //     color: "#f70233",
+  //     ageRange: [13, 14, 15],
+  //   },
+  // ];
 
   return (
     <>
@@ -49,18 +49,15 @@ export default function Home() {
         />
       </Head>
 
-      <div
-        style={{
-          backgroundColor: `${configurations[currentConfigIndex].color}`,
-        }}>
+      <div>
         <div className="flex justify-center items-center overflow-auto">
           <div
             className="w-full rounded-lg mx-auto"
             style={{ maxWidth: "90%" }}>
             <Main
-              // configurations={configurations}
-              // currentConfigIndex={currentConfigIndex}
-              // setCurrentConfigIndex={setCurrentConfigIndex}
+            // configurations={configurations}
+            // currentConfigIndex={currentConfigIndex}
+            // setCurrentConfigIndex={setCurrentConfigIndex}
             />
           </div>
         </div>
