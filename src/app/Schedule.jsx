bd133ae9a4777d7dc9e778d252b46b1df2bb7ce0
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-23 05:54:50 +0000

    Thu, May 23, 2024, 5:54 AM +00:00

--- src/app/Schedule.jsx
+++ src/app/Schedule.jsx
@@ -59,10 +59,17 @@ function Schedule() {
       .sort(); // Sort the segments to ensure proper order
 
     return segments.map((segment) => (
-      <div key={segment} className="mb-4 p-4 border rounded-lg shadow-sm bg-white">
+      <div
+        key={segment}
+        className="mb-4 p-4 border rounded-lg shadow-sm bg-white">
         <h3 className="text-xl font-semibold">{scheduleData[segment]}</h3>
-        <p><strong>Location:</strong> {scheduleData[`${segment}loc`]}</p>
-        <p><strong>Time:</strong> {scheduleData[`${segment}start`]} -  {scheduleData[`${segment}end`]}</p>
+        <p>
+          <strong>Location:</strong> {scheduleData[`${segment}loc`]}
+        </p>
+        <p>
+          <strong>Time:</strong> {scheduleData[`${segment}start`]} -{" "}
+          {scheduleData[`${segment}end`]}
+        </p>
       </div>
     ));
   };
@@ -73,53 +80,57 @@ function Schedule() {
         backgroundColor: `${configurations[currentConfigIndex].color}`,
       }}
       className="flex flex-col items-center">
-      <Menu
-        as="div"
-        className="relative inline-block justify-center text-center mt-4">
-        <div>
-          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-bold text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
-            <h2 className="text-4xl font-bold">
-              {configurations[currentConfigIndex].name}
-            </h2>
-            <ChevronDownIcon
-              className="ml-2 -mr-1 h-10 w-10"
-              aria-hidden="true"
-            />
-          </Menu.Button>
+      <div className="flex justify-center items-center overflow-auto">
+        <div className="w-full rounded-lg mx-auto" style={{ maxWidth: "90%" }}>
+          <Menu
+            as="div"
+            className="relative inline-block justify-center text-center mt-4">
+            <div>
+              <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-bold text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
+                <h2 className="text-4xl font-bold">
+                  {configurations[currentConfigIndex].name}
+                </h2>
+                <ChevronDownIcon
+                  className="ml-2 -mr-1 h-10 w-10"
+                  aria-hidden="true"
+                />
+              </Menu.Button>
+            </div>
+
+            <Transition
+              as={React.Fragment}
+              enter="transition ease-out duration-200"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95">
+              <Menu.Items className="absolute mt-2 origin-top divide-y divide-gray-100 rounded-lg bg-gradient-to-b from-gray-100 to-white shadow-xl ring-1 ring-black/5 focus:outline-none flex flex-col items-center z-50">
+                {configurations.map((config, index) => (
+                  <Menu.Item key={index}>
+                    {({ active }) => (
+                      <button
+                        onClick={() => setCurrentConfigIndex(index)}
+                        className={`${
+                          active ? "bg-blue-500 text-white" : "text-gray-900"
+                        } flex w-full items-center rounded-lg px-4 py-4 text-2xl font-semibold hover:bg-blue-100 transition-colors duration-200`}>
+                        {config.name}
+                      </button>
+                    )}
+                  </Menu.Item>
+                ))}
+              </Menu.Items>
+            </Transition>
+          </Menu>
+
+          <div className="w-full max-w-md text-gray-700 bg-white p-5 border rounded-lg shadow-lg mx-auto">
+            {Object.keys(scheduleData).length > 0 ? (
+              renderSchedule()
+            ) : (
+              <p className="text-center font-bold text-xl">Loading...</p>
+            )}
+          </div>
         </div>
-
-        <Transition
-          as={React.Fragment}
-          enter="transition ease-out duration-200"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95">
-          <Menu.Items className="absolute mt-2 origin-top divide-y divide-gray-100 rounded-lg bg-gradient-to-b from-gray-100 to-white shadow-xl ring-1 ring-black/5 focus:outline-none flex flex-col items-center z-50">
-            {configurations.map((config, index) => (
-              <Menu.Item key={index}>
-                {({ active }) => (
-                  <button
-                    onClick={() => setCurrentConfigIndex(index)}
-                    className={`${
-                      active ? "bg-blue-500 text-white" : "text-gray-900"
-                    } flex w-full items-center rounded-lg px-4 py-4 text-2xl font-semibold hover:bg-blue-100 transition-colors duration-200`}>
-                    {config.name}
-                  </button>
-                )}
-              </Menu.Item>
-            ))}
-          </Menu.Items>
-        </Transition>
-      </Menu>
-
-      <div className="w-full max-w-md text-gray-700 bg-white p-5 border rounded-lg shadow-lg mx-auto">
-        {Object.keys(scheduleData).length > 0 ? (
-          renderSchedule()
-        ) : (
-          <p className="text-center font-bold text-xl">Loading...</p>
-        )}
       </div>
     </div>
   );
