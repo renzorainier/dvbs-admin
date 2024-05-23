15541c17c9ee03d8f1896e812c974bad042e685d
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-23 03:23:39 +0000

    Thu, May 23, 2024, 3:23 AM +00:00

---
+++ src/app/firebaseConfig3.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Firebase configuration for the second account
+const firebaseConfig3 = {
+  apiKey: "AIzaSyDrCeB6u0CC3d-NWLudPiPzARkVgot2HyY",
+  authDomain: "points-20cf8.firebaseapp.com",
+  projectId: "points-20cf8",
+  storageBucket: "points-20cf8.appspot.com",
+  messagingSenderId: "341348174938",
+  appId: "1:341348174938:web:e45f679f1dc18f25149c63",
+};
+
+// Initialize Firebase app for the second account with a different name
+const ap3 = initializeApp(firebaseConfig3, 'app2');
+export const db3 = getFirestore(app3);
