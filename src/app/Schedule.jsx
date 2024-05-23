696934a9b10dbff6b97abc4aa00833003a95fc73
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-23 06:28:23 +0000

    Thu, May 23, 2024, 6:28 AM +00:00

--- src/app/Schedule.jsx
+++ src/app/Schedule.jsx
@@ -126,7 +126,7 @@ function Schedule() {
         <div
           key={segment}
           className={`mb-4 p-4 border rounded-lg shadow-sm ${
-            isCurrent ? "bg-green-500 text-white" : "bg-white"
+            isCurrent ? "  {configurations[currentConfigIndex].color} text-white" : "bg-white"
           }`}>
           <h3 className="text-xl font-semibold">{scheduleData[segment]}</h3>
           <p>
