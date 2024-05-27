67e9f5450a2b44433cd1390f3eb9d41d06a9d7c8
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-27 12:50:24 +0000

    Mon, May 27, 2024, 12:50 PM +00:00

--- src/app/DailyRewards.jsx
+++ src/app/DailyRewards.jsx
@@ -42,8 +42,6 @@ function DailyRewards() {
   const [searchQuery, setSearchQuery] = useState("");
   const [showConfirmation, setShowConfirmation] = useState(false);
   const [studentToMarkAbsent, setStudentToMarkAbsent] = useState(null);
-  const [showBiblePopup, setShowBiblePopup] = useState(false);
-  const [studentToUpdateBible, setStudentToUpdateBible] = useState(null);
   const audioRef = useRef(null);
 
   const currentConfig = configurations[currentConfigIndex];
@@ -94,23 +92,18 @@ function DailyRewards() {
         currentConfig.dbPath.split("/")[1]
       );
       const newValue = primaryData[fieldToUpdate] ? "" : uploadTime;
-      const bibleField = `${fieldToUpdate}bible`;
 
       await updateDoc(docRef, {
         [fieldToUpdate]: newValue,
-        [bibleField]: newValue ? "" : false,
       });
 
       setPrimaryData((prevData) => ({
         ...prevData,
         [fieldToUpdate]: newValue,
-        [bibleField]: newValue ? "" : false,
       }));
 
       if (newValue) {
         playEnterSound();
-        setStudentToUpdateBible(fieldName);
-        setShowBiblePopup(true);
       }
     } catch (error) {
       console.error("Error updating Firebase: ", error);
@@ -118,32 +111,6 @@ function DailyRewards() {
 
     setShowConfirmation(false);
     setStudentToMarkAbsent(null);
-  };
-
-  const updateBibleStatus = async (fieldName, broughtBible) => {
-    try {
-      const docRef = doc(
-        db,
-        currentConfig.dbPath.split("/")[0],
-        currentConfig.dbPath.split("/")[1]
-      );
-      const dayLetter = getCurrentDayLetter();
-      const bibleField = `${fieldName.slice(0, 2)}${dayLetter}bible`;
-
-      await updateDoc(docRef, {
-        [bibleField]: broughtBible ? true : false,
-      });
-
-      setPrimaryData((prevData) => ({
-        ...prevData,
-        [bibleField]: broughtBible ? true : false,
-      }));
-    } catch (error) {
-      console.error("Error updating Firebase: ", error);
-    }
-
-    setShowBiblePopup(false);
-    setStudentToUpdateBible(null);
   };
 
   const getButtonColor = (fieldName) => {
@@ -215,29 +182,6 @@ function DailyRewards() {
         </div>
       )}
 
-      {showBiblePopup && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center">
-          <div className="fixed inset-0 bg-black opacity-50" />
-          <div className="bg-white rounded-lg p-5 shadow-md z-10 flex flex-col items-center">
-            <p className="mb-2">Did the student bring their Bible today?</p>
-            <div className="flex space-x-4">
-              <button
-                className="bg-green-500 text-white font-bold py-2 px-4 rounded"
-                onClick={() => updateBibleStatus(studentToUpdateBible, true)}
-              >
-                Yes
-              </button>
-              <button
-                className="bg-red-500 text-white font-bold py-2 px-4 rounded"
-                onClick={() => updateBibleStatus(studentToUpdateBible, false)}
-              >
-                No
-              </button>
-            </div>
-          </div>
-        </div>
-      )}
-
       <Menu as="div" className="relative inline-block justify-center text-center mt-4">
         <div>
           <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-bold text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
@@ -246,82 +190,83 @@ function DailyRewards() {
           </Menu.Button>
 
           <Transition
-      as={Fragment}
-      enter="transition ease-out duration-200"
-      enterFrom="transform opacity-0 scale-95"
-      enterTo="transform opacity-100 scale-100"
-      leave="transition ease-in duration-75"
-      leaveFrom="transform opacity-100 scale-100"
-      leaveTo="transform opacity-0 scale-95"
-    >
-      <Menu.Items className="absolute mt-2 origin-top divide-y divide-gray-100 rounded-lg bg-gradient-to-b from-gray-100 to-white shadow-xl ring-1 ring-black/5 focus:outline-none flex flex-col items-center z-50">
-        {configurations.map((config, index) => (
-          <Menu.Item key={index}>
-            {({ active }) => (
-              <button
-                onClick={() => setCurrentConfigIndex(index)}
-                className={`${
-                  active ? "bg-blue-500 text-white" : "text-gray-900"
-                } flex w-full items-center rounded-lg px-4 py-4 text-2xl font-semibold hover:bg-blue-100 transition-colors duration-200`}
-              >
-                {config.name}
-              </button>
-            )}
-          </Menu.Item>
-        ))}
-      </Menu.Items>
-    </Transition>
-  </Menu>
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
+          >
+            <Menu.Items className="absolute mt-2 origin-top divide-y divide-gray-100 rounded-lg bg-gradient-to-b from-gray-100 to-white shadow-xl ring-1 ring-black/5 focus:outline-none flex flex-col items-center z-50">
+              {configurations.map((config, index) => (
+                <Menu.Item key={index}>
+                  {({ active }) => (
+                    <button
+                      onClick={() => setCurrentConfigIndex(index)}
+                      className={`${
+                        active ? "bg-blue-500 text-white" : "text-gray-900"
+                      } flex w-full items-center rounded-lg px-4 py-4 text-2xl font-semibold hover:bg-blue-100 transition-colors duration-200`}
+                    >
+                      {config.name}
+                    </button>
+                  )}
+                </Menu.Item>
+              ))}
+            </Menu.Items>
+          </Transition>
+        </Menu>
 
-
-  <div className="w-full max-w-md text-gray-700 bg-white p-5 border rounded-lg shadow-lg mx-auto">
-    <input
-      type="text"
-      placeholder="Search names..."
-      value={searchQuery}
-      onChange={(e) => setSearchQuery(e.target.value)}
-      className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 mb-4"
-    />
-    <div className="flex flex-col gap-4">
-      {filteredNames.map((name, index) => {
-        const studentIndex = Object.keys(primaryData).find(
-          (key) => primaryData[key] === name
-        );
-
-        return (
-          <div key={index} className="flex items-center">
-            <button
-              className={`w-70percent hover:bg-blue-700 text-white font-bold py-2 px-3 rounded-lg ${getButtonColor(
-                studentIndex
-              )}`}
-              onClick={() => {
-              handleClick(studentIndex);
-              }}>
-              {name}
-              </button>
-
-              <div className="flex flex-row ml-1">
-                {["A", "B", "C", "D", "E"].map((dayLetter) => {
-                  const fieldName = `${studentIndex.slice(0, 2)}${dayLetter}`;
-                  return (
-                    <div
-                      key={dayLetter}
-                      className={`w-4 h-9 rounded-lg ${
-                        primaryData[fieldName]
-                          ? config.colors.present
-                          : config.colors.absent
-                      } mr-1`}
-                    ></div>
-                  );
-                })}
-              </div>
-              </div>
+        <div className="w-full max-w-md text-gray-700 bg-white p-5 border rounded-lg shadow-lg mx-auto">
+          <input
+            type="text"
+            placeholder="Search names..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 mb-4"
+          />
+          <div className="flex flex-col gap-4">
+            {filteredNames.map((name, index) => {
+              const studentIndex = Object.keys(primaryData).find(
+                (key) => primaryData[key] === name
               );
-              })}
-              </div>
-              </div>
-              <audio ref={audioRef} />
-              </div>
+
+              return (
+                <div key={index} className="flex items-center">
+                  <button
+                    className={`w-70percent hover:bg-blue-700 text-white font-bold py-2 px-3 rounded-lg ${getButtonColor(
+                      studentIndex
+                    )}`}
+                    onClick={() => {
+                      handleClick(studentIndex);
+                    }}
+                  >
+                    {name}
+                  </button>
+
+                  <div className="flex flex-row ml-1">
+                    {["A", "B", "C", "D", "E"].map((dayLetter) => {
+                      const fieldName = `${studentIndex.slice(0, 2)}${dayLetter}`;
+                      return (
+                        <div
+                          key={dayLetter}
+                          className={`w-4 h-9 rounded-lg ${
+                            primaryData[fieldName]
+                              ? config.colors.present
+                              : config.colors.absent
+                          } mr-1`}
+                        ></div>
+                      );
+                    })}
+                  </div>
+                </div>
               );
-              }
-              export default DailyRewards;
+            })}
+          </div>
+        </div>
+        <audio ref={audioRef} />
+      </div>
+    );
+  }
+  export default DailyRewards;
+
