17bf4d51eead114d711b4faba78f63756e369a9d
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-28 02:16:08 +0000

    Tue, May 28, 2024, 2:16 AM +00:00

--- src/app/DailyRewards.jsx
+++ src/app/DailyRewards.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import { doc, updateDoc, getDoc } from "firebase/firestore";
-import { db } from "./firebase.js"; // Import your Firebase config
-import Confetti from "react-confetti";
+import { db } from "./firebase.js";
 import { Menu, Transition } from "@headlessui/react";
 import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
@@ -78,13 +77,14 @@ function DailyRewards() {
           currentConfig.dbPath.split("/")[1]
         );
 
-        await updateDoc(docRef, { [fieldToUpdate]: true });
+        // Toggle the field value
+        const newValue = !primaryData[fieldToUpdate];
 
-        console.log("done");
+        await updateDoc(docRef, { [fieldToUpdate]: newValue });
 
         setPrimaryData((prevData) => ({
           ...prevData,
-          [fieldToUpdate]: true,
+          [fieldToUpdate]: newValue,
         }));
       } catch (error) {
         console.error("Error updating document: ", error);
@@ -113,158 +113,76 @@ function DailyRewards() {
   return (
     <div
       className="h-screen overflow-auto"
-      style={{ backgroundColor: currentConfig.color }}>
+      style={{ backgroundColor: currentConfig.color }}
+    >
       <div className="flex justify-center items-center overflow-auto">
         <div className="w-full rounded-lg mx-auto " style={{ maxWidth: "90%" }}>
           <div className="flex flex-col gap-4">
-            <div className="w-full">
-              <Menu as="div" className="relative inline-block mt-4">
-                <div>
-                  <Menu.Button className="inline-flex justify-center w-full rounded-md bg-black/20 px-4 py-2 text-sm font-bold text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
-                    <h2 className="text-4xl font-bold">
-                      {configurations[currentConfigIndex].name}
-                    </h2>
-                    <ChevronDownIcon
-                      className="ml-2 -mr-1 h-10 w-10"
-                      aria-hidden="true"
-                    />
-                  </Menu.Button>
-                </div>
-                <Transition
-                  as={React.Fragment}
-                  enter="transition ease-out duration-200"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95">
-                  <Menu.Items className="absolute mt-2 origin-top divide-y divide-gray-100 rounded-lg bg-gradient-to-b from-gray-100 to-white shadow-xl ring-1 ring-black/5 focus:outline-none flex flex-col items-center z-50">
-                    {configurations.map((config, index) => (
-                      <Menu.Item key={index}>
-                        {({ active }) => (
-                          <button
-                            onClick={() => setCurrentConfigIndex(index)}
-                            className={`${
-                              active
-                                ? "bg-blue-500 text-white"
-                                : "text-gray-900"
-                            } flex w-full items-center rounded-lg px-4 py-4 text-2xl font-semibold hover:bg-blue-100 transition-colors duration-200`}>
-                            {config.name}
-                          </button>
-                        )}
-                      </Menu.Item>
-                    ))}
-                  </Menu.Items>
-                </Transition>
-              </Menu>
-            </div>
+            {/* Dropdown for selecting configuration */}
+            {/* Dropdown for selecting field to modify */}
+            <div className="w-full max-w-md text-gray-700 bg-white p-5 border rounded-lg shadow-lg mx-auto">
+              {/* Search input */}
+              <input
+                type="text"
+                placeholder="Search names..."
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 mb-4"
+              />
+              {/* Render student names */}
+              <div className="flex flex-col gap-4 w-full max-w-md">
+                {filteredNames.map((name, index) => {
+                  const studentIndex = Object.keys(primaryData).find(
+                    (key) => primaryData[key] === name
+                  );
 
-            <div className="w-full">
-              <Menu as="div" className="relative inline-block mb-4">
-                <div>
-                  <Menu.Button className="inline-flex justify-center w-full rounded-md bg-black/20 px-4 py-2 text-sm font-bold text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75">
-                    <h2 className="text-2xl font-bold">
-                      {selectedField
-                        ? selectedField
-                            .replace(/([A-Z])/g, " $1")
-                            .trim()
-                            .replace(/\b\w/g, (char) => char.toUpperCase())
-                        : "Select Field to Modify"}
-                    </h2>
-                    <ChevronDownIcon
-                      className="ml-2 -mr-1 h-8 w-10"
-                      aria-hidden="true"
-                    />
-                  </Menu.Button>
-                </div>
-                <Transition
-                  as={React.Fragment}
-                  enter="transition ease-out duration-200"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95">
-                  <Menu.Items className="absolute mt-2 origin-top divide-y divide-gray-100 rounded-lg bg-gradient-to-b from-gray-100 to-white shadow-xl ring-1 ring-black/5 focus:outline-none flex flex-col items-center z-50">
-                    {currentConfig.fields.map((field, index) => (
-                      <Menu.Item key={index}>
-                        {({ active }) => (
-                          <button
-                            onClick={() => setSelectedField(field)}
-                            className={`${
-                              active
-                                ? "bg-blue-500 text-white"
-                                : "text-gray-900"
-                            } flex w-full items-center rounded-lg px-4 py-4 text-2xl font-semibold hover:bg-blue-100 transition-colors duration-200`}>
-                            {field
-                              .replace(/([A-Z])/g, " $1")
-                              .trim()
-                              .replace(/\b\w/g, (char) => char.toUpperCase())}
-                          </button>
-                        )}
-                      </Menu.Item>
-                    ))}
-                  </Menu.Items>
-                </Transition>
-              </Menu>
-            </div>
-          </div>
-
-          <div className="w-full max-w-md text-gray-700 bg-white p-5 border rounded-lg shadow-lg mx-auto">
-            <input
-              type="text"
-              placeholder="Search names..."
-              value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-              className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 mb-4"
-            />
-            <div className="flex flex-col gap-4 w-full max-w-md">
-              {filteredNames.map((name, index) => {
-                const studentIndex = Object.keys(primaryData).find(
-                  (key) => primaryData[key] === name
-                );
-
-                return (
-                  <div
-                    key={index}
-                    className="flex items-center justify-between">
-                    <button
-                      className="flex-grow hover:bg-blue-700 text-white font-bold py-2 px-3 rounded-lg"
-                      style={{
-                        backgroundColor: primaryData[
-                          `${studentIndex.slice(
+                  return (
+                    <div
+                      key={index}
+                      className="flex items-center justify-between"
+                    >
+                      {/* Button for marking/unmarking */}
+                      <button
+                        className="flex-grow hover:bg-blue-700 text-white font-bold py-2 px-3 rounded-lg"
+                        style={{
+                          backgroundColor: primaryData[
+                            `${studentIndex.slice(
+                              0,
+                              2
+                            )}${getCurrentDayLetter()}${selectedField}`
+                          ]
+                            ? currentConfig.color
+                            : "#E5E7EB",
+                        }}
+                        onClick={() => {
+                          handleClick(studentIndex);
+                        }}
+                      >
+                        {name}
+                      </button>
+                      {/* Indicator */}
+                      <div className="flex ml-1">
+                        {["A", "B", "C", "D", "E"].map((dayLetter) => {
+                          const fieldName = `${studentIndex.slice(
                             0,
                             2
-                          )}${getCurrentDayLetter()}${selectedField}`
-                        ]
-                          ? currentConfig.color
-                          : "#E5E7EB",
-                      }}
-                      onClick={() => {
-                        handleClick(studentIndex);
-                      }}>
-                      {name}
-                    </button>
-                    <div className="flex ml-1">
-                      {["A", "B", "C", "D", "E"].map((dayLetter) => {
-                        const fieldName = `${studentIndex.slice(
-                          0,
-                          2
-                        )}${dayLetter}${selectedField}`;
-                        const indicatorColor = primaryData[fieldName]
-                          ? currentConfig.color
-                          : "#E5E7EB"; // Use currentConfig.color for the indicator color
-                        return (
-                          <div
-                            key={dayLetter}
-                            className="w-4 h-9 rounded-lg mr-1"
-                            style={{ backgroundColor: indicatorColor }}></div>
-                        );
-                      })}
+                          )}${dayLetter}${selectedField}`;
+                          const indicatorColor = primaryData[fieldName]
+                            ? currentConfig.color
+                            : "#E5E7EB";
+                          return (
+                            <div
+                              key={dayLetter}
+                              className="w-4 h-9 rounded-lg mr-1"
+                              style={{ backgroundColor: indicatorColor }}
+                            ></div>
+                          );
+                        })}
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </div>
             </div>
           </div>
         </div>
