6f7bc4a03d4f09d01adfc8bd9eb785625bce5c5e
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-30 04:21:38 +0000

    Thu, May 30, 2024, 4:21 AM +00:00

--- src/app/InvitedByField.jsx
+++ src/app/InvitedByField.jsx
@@ -4,32 +4,54 @@ import TeacherCombobox from "./TeacherCombobox";
 
 function InvitedByField({ invitedBy, handleInputChange, config }) {
   const [isStudent, setIsStudent] = useState(true);
+  const [selectedDocument, setSelectedDocument] = useState("dvbs/primary");
 
   const handleToggle = () => {
     setIsStudent(!isStudent);
     handleInputChange({ target: { value: "" } }, "invitedBy");
   };
 
+  const documentPaths = ["dvbs/primary", "dvbs/middlers", "dvbs/juniors", "dvbs/youth"];
+
+  const handleDocumentChange = (documentPath) => {
+    setSelectedDocument(documentPath);
+  };
+
   return (
     <div className="space-y-4">
-      <button
-        className={`bg-[${config.color}] text-white font-semibold py-3 px-6 rounded-lg mt-4 w-full flex items-center justify-center transition duration-300 ease-in-out`}
-        onClick={handleToggle}>
-        {isStudent ? "Switch to Teacher" : "Switch to Student"}
-      </button>
-      {isStudent ? (
+      <div className="flex space-x-4">
+        <button
+          className={`bg-[${config.color}] text-white font-semibold py-3 px-6 rounded-lg w-full flex items-center justify-center transition duration-300 ease-in-out`}
+          onClick={handleToggle}
+        >
+          {isStudent ? "Switch to Teacher" : "Switch to Student"}
+        </button>
+      </div>
+      <div className="flex space-x-4">
+        {documentPaths.map((documentPath) => (
+          <button
+            key={documentPath}
+            className={`bg-[${config.color}] text-white font-semibold py-3 px-6 rounded-lg w-full flex items-center justify-center transition duration-300 ease-in-out`}
+            onClick={() => handleDocumentChange(documentPath)}
+            style={{ backgroundColor: selectedDocument === documentPath ? "gray" : "" }}
+          >
+            {documentPath.split("/")[1].charAt(0).toUpperCase() + documentPath.split("/")[1].slice(1)}
+          </button>
+        ))}
+      </div>
+      {!isStudent ? (
+        <TeacherCombobox
+          invitedBy={invitedBy}
+          handleInputChange={handleInputChange}
+          config={config}
+        />
+      ) : (
         <input
           type="text"
           value={invitedBy}
           onChange={(e) => handleInputChange(e, "invitedBy")}
           placeholder="Invited by Student"
           className={`border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:border-${config.color}`}
-        />
-      ) : (
-        <TeacherCombobox
-          invitedBy={invitedBy}
-          handleInputChange={handleInputChange}
-          config={config}
         />
       )}
     </div>
