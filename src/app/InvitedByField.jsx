9075b200886377beb57de70cd88309cb811cded6
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-30 03:38:28 +0000

    Thu, May 30, 2024, 3:38 AM +00:00

--- src/app/InvitedByField.jsx
+++ src/app/InvitedByField.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 import { Combobox, Transition } from "@headlessui/react";
 
-const predefinedTeachers = ["Teacher A", "Teacher B", "Teacher C", "Teacher D"];
+const predefinedTeachers = ["Renz", "Vince", "Aljon", "Lance"];
 
 function InvitedByField({ invitedBy, handleInputChange, config }) {
   const [isStudent, setIsStudent] = useState(true);
   const [selectedTeacher, setSelectedTeacher] = useState("");
+  const [query, setQuery] = useState("");
 
   const handleToggle = () => {
     setIsStudent(!isStudent);
     handleInputChange({ target: { value: "" } }, "invitedBy");
   };
+
+  const filteredTeachers =
+    query === ""
+      ? predefinedTeachers
+      : predefinedTeachers.filter((teacher) =>
+          teacher.toLowerCase().includes(query.toLowerCase())
+        );
 
   return (
     <div className="space-y-4">
@@ -35,7 +43,10 @@ function InvitedByField({ invitedBy, handleInputChange, config }) {
           <div className="relative">
             <Combobox.Input
               className={`border border-gray-300 rounded-lg px-4 py-2 w-full focus:outline-none focus:border-${config.color}`}
-              onChange={(e) => handleInputChange(e, "invitedBy")}
+              onChange={(event) => {
+                setQuery(event.target.value);
+                handleInputChange(event, "invitedBy");
+              }}
               displayValue={(teacher) => teacher}
               placeholder="Select a Teacher"
             />
@@ -46,7 +57,7 @@ function InvitedByField({ invitedBy, handleInputChange, config }) {
               leaveTo="opacity-0"
             >
               <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto bg-white rounded-md py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {predefinedTeachers.map((teacher) => (
+                {filteredTeachers.map((teacher) => (
                   <Combobox.Option
                     key={teacher}
                     className={({ active }) =>
