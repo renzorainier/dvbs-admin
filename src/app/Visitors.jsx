8658d557fb3c09901d16abb15c1dfec618ca9843
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-31 05:27:18 +0000

    Fri, May 31, 2024, 5:27 AM +00:00

--- src/app/Visitors.jsx
+++ src/app/Visitors.jsx
@@ -390,7 +390,7 @@ function Visitors({ config, currentConfigIndex, setCurrentConfigIndex }) {
                 />
               </div>
             </div>
-            <div className="flex items-center space-x-4">
+            <div className="flex items-center justify-center space-x-4">
               <button
                 className={`flex items-center space-x-2 px-3 py-2 rounded-lg ${
                   broughtBible
