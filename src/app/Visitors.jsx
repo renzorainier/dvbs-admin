0496216db596fd39a8228a44a6483bd00f1fac69
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-31 03:34:28 +0000

    Fri, May 31, 2024, 3:34 AM +00:00

--- src/app/Visitors.jsx
+++ src/app/Visitors.jsx
@@ -382,7 +382,7 @@ function Visitors({ config, currentConfigIndex, setCurrentConfigIndex }) {
               />
             </div>
 
-      <div className="w-full mt-5 py-5 bg-[#9ca3af] shadow-md rounded-lg border overflow-hidden mx-auto">
+      <div className="w-full mt-5 py-5 bg-[#9ca3af] shadow-md rounded-lg border  mx-auto">
         <div className="p-6">
 
           <InvitedByField
