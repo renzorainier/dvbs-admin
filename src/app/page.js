e0e5d86778d06928a61689055e3772553853bfc9
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-31 09:17:04 +0000

    Fri, May 31, 2024, 9:17 AM +00:00

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,10 @@ export default function Home() {
       <Head>
         <title>Attendance</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link
+          href="https://fonts.googleapis.com/css2?family=Urbanist:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400&amp;display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <div>
