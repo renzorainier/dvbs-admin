d39e011487fa5787ed8972f22eab57663266afb8
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-05-31 03:02:22 +0000

    Fri, May 31, 2024, 3:02 AM +00:00

--- src/app/Visitors.jsx
+++ src/app/Visitors.jsx
@@ -315,7 +315,7 @@ function Visitors({ config, currentConfigIndex, setCurrentConfigIndex }) {
             </div>
 
             <div className="flex items-center ">
-              <Menu as="div" className="relative inline-block  w-full z-10">
+              <Menu as="div" className="relative   w-full z-50">
                 <div>
                   <Menu.Button className="inline-flex rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700">
                     {age ? `Age: ${age}` : "Select Age"}
