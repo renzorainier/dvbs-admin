7704deb8e2efff8550bfc13a76b5ea86ddb8ef95
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-01 07:27:34 +0000

    Sat, Jun 1, 2024, 7:27 AM +00:00

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,16 +10,26 @@ const urlsToCache = [
 
 self.addEventListener('install', event => {
   console.log('Service Worker installing.');
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Cache opened');
+      return cache.addAll(urlsToCache);
+    })
+  );
+
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   console.log('Service Worker activating.');
+
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
             return caches.delete(cacheName);
           }
         })
@@ -29,15 +39,24 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  console.log('Fetch event:', event.request.url);
+
   event.respondWith(
     caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      if (response) {
+        console.log('Cache hit:', event.request.url);
+        return response;
+      }
+
+      console.log('Cache miss:', event.request.url);
+      return fetch(event.request);
     })
   );
 });
 
 self.addEventListener('message', event => {
   if (event.data === 'SKIP_WAITING') {
+    console.log('Received SKIP_WAITING message from client.');
     self.skipWaiting();
   }
 });
