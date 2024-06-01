befcffb2f02f64908009ca31e14ef678bb1d8907
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-01 07:20:21 +0000

    Sat, Jun 1, 2024, 7:20 AM +00:00

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -18,7 +18,13 @@ export const db = getFirestore(app);
 
 
 
-
+// dvbs2
+// apiKey: "AIzaSyBlN6qVKKC2rMiCoOa-5GZuK2RGr3hOk-s",
+// authDomain: "dvbs2-ca434.firebaseapp.com",
+// projectId: "dvbs2-ca434",
+// storageBucket: "dvbs2-ca434.appspot.com",
+// messagingSenderId: "198606446381",
+// appId: "1:198606446381:web:ef63f2174e0735004c3da4"
 
 
 //firebase config 1 (original)
@@ -27,4 +33,4 @@ export const db = getFirestore(app);
 // projectId: "dvbs-f6778",
 // storageBucket: "dvbs-f6778.appspot.com",
 // messagingSenderId: "970229367594",
-// appId: "1:970229367594:web:3b65a45ad74ba0f951d453",  
+// appId: "1:970229367594:web:3b65a45ad74ba0f951d453",
