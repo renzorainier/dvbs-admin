960d4063ef3f7a1e7686da82340c7fbdcea28821
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-01 13:31:24 +0000

    Sat, Jun 1, 2024, 1:31 PM +00:00

--- src/app/StudentOutTime.jsx
+++ src/app/StudentOutTime.jsx
@@ -77,8 +77,15 @@ function StudentOutTime() {
   const getCurrentDayLetter = () => {
     const days = ["A", "B", "C", "D", "E"];
     const dayIndex = new Date().getDay();
-    return days[dayIndex === 0 ? 6 : dayIndex - 1];
+    return days[dayIndex >= 1 && dayIndex <= 5 ? dayIndex - 1 : 4];
   };
+
+
+  // const getCurrentDayLetter = () => {
+  //   const days = ["A", "B", "C", "D", "E", "F", "G"];
+  //   const dayIndex = new Date().getDay();
+  //   return days[dayIndex === 0 ? 6 : dayIndex - 1];
+  // };
 
   const handleClick = (groupId, prefix, inTimeField, outTimeField, outTime) => {
     if (outTime) {
