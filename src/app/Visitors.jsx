cf44c044146494305efa8097d60f43232ebe6181
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-03 23:19:47 +0000

    Mon, Jun 3, 2024, 11:19 PM +00:00

--- src/app/Visitors.jsx
+++ src/app/Visitors.jsx
@@ -435,7 +435,11 @@ function Visitors({ config, currentConfigIndex, setCurrentConfigIndex, isVisitor
             </div>
             <button
               className={`bg-[${config.color}] gap-2 text-white font-semibold py-3 px-6 rounded-lg mt-4 w-full flex items-center justify-center transition duration-300 ease-in-out`}
-              onClick={addVisitor}>
+              onClick={if (!isVisitorView) {
+                {addVisitor};
+              } else {
+                setShowVisitorPrompt(true); // Show visitor prompt if in visitor view
+              }}>
               <IoMdPersonAdd />
               Add Visitor
             </button>
