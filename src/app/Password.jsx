36f69c444f2dbb3f3af46fa9772f3ba8d080762d
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-03 22:12:37 +0000

    Mon, Jun 3, 2024, 10:12 PM +00:00

--- src/app/Password.jsx
+++ src/app/Password.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 
 const Password = ({ correctPassword, children }) => {
-  const [password, setPassword] = useState("");
+  const [pin, setPin] = useState("");
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
+  const handlePinChange = (event) => {
+    setPin(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (password === correctPassword) {
+    if (pin === correctPassword) {
       setIsAuthenticated(true);
     } else {
-      alert("Incorrect password. Please try again.");
+      alert("Incorrect PIN. Please try again.");
+      setPin(""); // Clear PIN input after incorrect entry
     }
   };
 
@@ -22,19 +23,20 @@ const Password = ({ correctPassword, children }) => {
       {isAuthenticated ? (
         children
       ) : (
-        <div className="h-screen flex justify-center items-center">
-          <div className="bg-gray-100 p-8 rounded-lg">
+        <div className="h-screen flex justify-center items-center bg-gray-200">
+          <div className="bg-white p-8 rounded-lg shadow-lg">
+            <h2 className="text-lg font-semibold mb-4">Enter PIN</h2>
             <form onSubmit={handleSubmit}>
               <input
-                type="password"
-                value={password}
-                onChange={handlePasswordChange}
-                className="border border-gray-400 rounded px-3 py-2 mb-4 block w-full"
-                placeholder="Enter password"
+                type="number"
+                value={pin}
+                onChange={handlePinChange}
+                className="border border-gray-300 rounded px-3 py-2 mb-4 block w-full text-lg"
+                placeholder="Enter PIN"
               />
               <button
                 type="submit"
-                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded block w-full">
                 Submit
               </button>
             </form>
