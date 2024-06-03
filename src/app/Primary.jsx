32da8f2e4ea7d26bf0e198f76d1f5979ef392446
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-03 23:06:25 +0000

    Mon, Jun 3, 2024, 11:06 PM +00:00

--- src/app/Primary.jsx
+++ src/app/Primary.jsx
@@ -328,24 +328,6 @@ function Primary({
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875"
-            />
-          </svg>
-          <p className="text-gray-800 font-bold ml-2 text-lg sm:text-base md:text-lg lg:text-xl">
-            {countPresentForToday()}
-          </p>
-        </div>
-        <div className="flex items-center bg-white border rounded-lg shadow-md p-4 ml-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
               d="M6 18L18 6M6 6l12 12"
             />
           </svg>
