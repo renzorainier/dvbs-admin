5f81a3e31ee9ec9bbc698126408151d186bb99c0
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-04 00:40:04 +0000

    Tue, Jun 4, 2024, 12:40 AM +00:00

--- src/app/Password.jsx
+++ src/app/Password.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 
-const Password = ({ correctPassword, children, isVisitorView, setIsVisitorView }) => {
+const Password = ({
+  correctPassword,
+  children,
+  isVisitorView,
+  setIsVisitorView,
+}) => {
   const [pin, setPin] = useState("");
   const [error, setError] = useState("");
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-//   const [isVisitorView, setIsVisitorView] = useState(false);
+  //   const [isVisitorView, setIsVisitorView] = useState(false);
 
   const handlePinChange = (event) => {
     setPin(event.target.value);
@@ -31,31 +36,41 @@ const Password = ({ correctPassword, children, isVisitorView, setIsVisitorView }
       {isAuthenticated || isVisitorView ? (
         children
       ) : (
-        <div className="h-screen flex justify-center items-center ">
-          <div className="bg-white p-8 rounded-lg shadow-lg">
-            <h1>
-              This is a restricted feature intended only for specific persons, enter password to edit data or view as visitor.
-            </h1>
-            <form onSubmit={handleSubmit}>
-              <input
-                type="number"
-                value={pin}
-                onChange={handlePinChange}
-                className="border border-gray-300 rounded px-3 py-2 mb-4 block w-full text-lg"
-                placeholder="Enter PIN"
-              />
-              {error && <p className="text-red-500 text-sm mb-4">{error}</p>} {/* Display error message */}
-              <button
-                type="submit"
-                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded block w-full">
-                Submit
-              </button>
-              <button
-                onClick={handleVisitorView}
-                className="bg-gray-500 hover:bg-gray-600 text-white font-semibold py-2 px-4 rounded block w-full mt-4">
-                Enter as Visitor
-              </button>
-            </form>
+        <div className="flex justify-center items-center overflow-auto">
+          <div
+            className="w-full rounded-lg mx-auto"
+            style={{ maxWidth: "90%" }}>
+            <div className="h-screen flex justify-center items-center ">
+              <div className="bg-white p-8 rounded-lg shadow-lg">
+                <h1>
+                  This is a restricted feature intended only for specific
+                  persons, enter password to edit data or view as a visitor.
+                </h1>
+                <form onSubmit={handleSubmit}>
+                  <input
+                    type="number"
+                    value={pin}
+                    onChange={handlePinChange}
+                    className="border border-gray-300 rounded px-3 py-2 mb-4 block w-full text-lg"
+                    placeholder="Enter PIN"
+                  />
+                  {error && (
+                    <p className="text-red-500 text-sm mb-4">{error}</p>
+                  )}{" "}
+                  {/* Display error message */}
+                  <button
+                    type="submit"
+                    className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded block w-full">
+                    Submit
+                  </button>
+                  <button
+                    onClick={handleVisitorView}
+                    className="bg-gray-500 hover:bg-gray-600 text-white font-semibold py-2 px-4 rounded block w-full mt-4">
+                    Enter as Visitor
+                  </button>
+                </form>
+              </div>
+            </div>
           </div>
         </div>
       )}
