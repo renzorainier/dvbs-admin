31e371d19b53403af93f7465a1f3d9bb78701e18
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-04 01:42:40 +0000

    Tue, Jun 4, 2024, 1:42 AM +00:00

--- src/app/Primary.jsx
+++ src/app/Primary.jsx
@@ -358,11 +358,7 @@ function Primary({
                   className={`w-70percent flex items-center justify-center hover:bg-blue-700 text-white font-bold py-2 px-3 rounded-lg ${getButtonColor(
                     studentIndex
                   )}`}
-                  onClick={(      if (!isVisitorView) {
-                      handleClick(studentIndex);
-                    } else {
-                      setShowVisitorPrompt(true); // Show visitor prompt if in visitor view
-                    }) => {
+                  onClick={() => {
                     if (!isVisitorView) {
                       handleClick(studentIndex);
                     } else {
