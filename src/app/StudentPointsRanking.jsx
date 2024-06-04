ece1bc8811ec044d31413312f789a0fbca2b512b
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-04 14:04:10 +0000

    Tue, Jun 4, 2024, 2:04 PM +00:00

--- src/app/StudentPointsRanking.jsx
+++ src/app/StudentPointsRanking.jsx
@@ -80,7 +80,7 @@ const StudentRanking = () => {
                   className="flex-1 text-white font-bold py-2 px-4 rounded-lg bg-gray-400 hover:bg-gray-700"
                   onClick={() => {}}
                 >
-                  {student.name}
+                  {student.name} - {student.points} points
                 </button>
 
                 {/* Add colored div */}
