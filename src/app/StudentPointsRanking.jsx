e13fe0044e3f08610f43c9d20683060e683ddaa5
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-05 03:43:33 +0000

    Wed, Jun 5, 2024, 3:43 AM +00:00

--- src/app/StudentPointsRanking.jsx
+++ src/app/StudentPointsRanking.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, onSnapshot } from "firebase/firestore"; // Changed getDocs to onSnapshot
 import { db } from "./firebase.js";
 
 const StudentRanking = () => {
@@ -88,20 +88,9 @@ const StudentRanking = () => {
       setLoading(false);
     });
 
-    // Log screen size on mount
-    setScreenSize({ width: window.innerWidth, height: window.innerHeight });
-
-    // Log screen size on resize
-    const handleResize = () => {
-      setScreenSize({ width: window.innerWidth, height: window.innerHeight });
-    };
-    window.addEventListener("resize", handleResize);
-
     return () => {
       // Unsubscribe from the snapshot listener when the component unmounts
       unsubscribe();
-      // Remove event listener on unmount
-      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -127,7 +116,7 @@ const StudentRanking = () => {
   };
 
   if (loading) {
-    return <div>Loading...</div>;
+    return <div></div>;
   }
 
   return (
