7db455f5342592512b165dfcbe666605a07ae4e7
renzorainier <114910204+renzorainier@users.noreply.github.com>
2024-06-05 07:26:46 +0000

    Wed, Jun 5, 2024, 7:26 AM +00:00

--- src/app/StudentPointsRanking.jsx
+++ src/app/StudentPointsRanking.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { collection, onSnapshot, doc } from "firebase/firestore";
 import { db } from "./firebase.js";
 import { db2 } from "./firebaseConfig2.js";
-import { Transition } from '@headlessui/react';
 
 const StudentRanking = () => {
   const [groupedStudents, setGroupedStudents] = useState({});
@@ -130,43 +129,25 @@ const StudentRanking = () => {
 
   return (
     <div className="bg-[#9ca3af] min-h-screen h-screen overflow-auto">
-    <div className="flex justify-center items-center h-full overflow-auto">
-      <div className="w-full h-full rounded-lg mx-auto flex flex-col justify-center">
-        {configGroup &&
-          groupedStudents[configGroup] && (
-            <div
-              key={configGroup}
-              className="w-full text-center text-gray-700 bg-white p-5 border rounded-lg shadow-lg flex-grow"
-            >
-              <h1 className="text-9xl font-bold mb-4">Highest points </h1>
-              <h4 className="text-5xl font-bold mb-4"> {configGroup}</h4>
-              <div className="flex flex-col justify-between">
-                {Object.keys(groupedStudents[configGroup]).map(
-                  (rank, index) =>
-                    parseInt(rank) <= 5 && (
-                      <Transition
-                        key={rank}
-                        show={true}
-                        appear={true}
-                        enter={`transform transition ease-in-out duration-500 delay-${index + 1}00`}
-                        enterFrom="translate-x-full"
-                        enterTo="translate-x-0"
-                        leave="transform transition ease-in-out duration-500"
-                        leaveFrom="translate-x-0"
-                        leaveTo="translate-x-full"
-                      >
+      <div className="flex justify-center items-center h-full overflow-auto">
+        <div className="w-full h-full rounded-lg mx-auto flex flex-col justify-center">
+          {configGroup &&
+            groupedStudents[configGroup] && ( // Check if configGroup is fetched and if the corresponding group data exists
+              <div
+                key={configGroup}
+                className="w-full text-center text-gray-700 bg-white p-5 border rounded-lg shadow-lg flex-grow">
+                <h1 className="text-9xl font-bold mb-4">Highest points </h1>
+                <h4 className="text-5xl font-bold mb-4"> {configGroup}</h4>
+                <div className="flex flex-col justify-between ">
+                  {Object.keys(groupedStudents[configGroup]).map(
+                    (rank) =>
+                      parseInt(rank) <= 5 && (
                         <div
                           key={rank}
-                          className={`flex items-center p-4 bg-gray-100 rounded-lg shadow-md mb-4 last:mb-0 flex-grow`}
-                          style={{
-                            transform: "translateX(100%)", // Start from the right side
-                            transitionDelay: `${index * 100}ms`,
-                          }}
-                        >
+                          className="flex items-center p-4 bg-gray-100 rounded-lg shadow-md mb-4 last:mb-0">
                           <div
                             className="text-9xl font-extrabold text-center text-black-700 flex-shrink-0"
-                            style={{ width: "120px" }}
-                          >
+                            style={{ width: "120px" }}>
                             {rank}
                           </div>
                           <div className="flex-grow">
@@ -175,16 +156,14 @@ const StudentRanking = () => {
                                 (student) => (
                                   <div
                                     key={`${student.id}-${student.prefix}`}
-                                    className="flex items-center m-2 w-full"
-                                  >
+                                    className="flex items-center m-2 w-full">
                                     <div
                                       className="flex-grow p-4 rounded-l-lg shadow-md text-white font-bold text-5xl"
                                       style={{
                                         backgroundColor: getBackgroundColor(
                                           student.group
                                         ),
-                                      }}
-                                    >
+                                      }}>
                                       {student.name}
                                     </div>
                                     <div className="flex-shrink-0 ml-auto bg-black p-4 rounded-r-lg shadow-md text-white font-bold text-5xl">
@@ -196,19 +175,15 @@ const StudentRanking = () => {
                             </div>
                           </div>
                         </div>
-                      </Transition>
-                    )
-                )}
+                      )
+                  )}
+                </div>
               </div>
-            </div>
-          )}
+            )}
+        </div>
       </div>
     </div>
-  </div>
-
-
   );
 };
 
 export default StudentRanking;
-
